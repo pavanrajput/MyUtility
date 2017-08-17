11149604dc10dd6a4de12a9f6e7be10effdc4271
pavan <pavan.rajput1987.m@gmail.com>
2017-08-17 13:55:17 +0530

    Initial Commit

---
+++ pavanutils/src/main/java/com/pavan/utils/Utility.java
@@ -0,0 +1,15 @@
+package com.pavan.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by pavan on 17/8/17.
+ */
+
+public class Utility {
+
+    public static void showToast(Context context){
+        Toast.makeText(context,"Tesing Library",Toast.LENGTH_SHORT).show();
+    }
+}
